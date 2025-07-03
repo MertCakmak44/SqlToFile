c078584327f57aa39d5b451309d1448260b1e042
Mert Çakmak <mert_cakmak077@hotmail.com>
2025-07-03 20:14:29 +0300

    düzeltme

--- MyApp.Core/Dtos/PurchaseCreateDto.cs
+++ MyApp.Core/Dtos/PurchaseCreateDto.cs
@@ -9,7 +9,7 @@ namespace MyAppCore.Dtos
     public class PurchaseCreateDto
     {
         public int StockId { get; set; }
-        public int Quantity { get; set; }
+        public int Amount { get; set; }
     }
 
 }
